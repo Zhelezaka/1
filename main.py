53addecca6f12b7c9800bccb01a35a7d51aaf31a
Zhelezaka <118340611+Zhelezaka@users.noreply.github.com>
2022-11-18 08:53:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ driver.get('https://www.google.ru/')
 e = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
 e.send_keys('купить ноутбук')
 e.send_keys(Keys.ENTER)
-driver.find_element(By.XPATH, '/html/body/div[7]/div/div[11]/div[2]/div[2]/div[2]/div/div/div[3]/div/div/div[1]/div/a').click()
+time.sleep(4)
+driver.find_element(By.XPATH, '/html/body/div[7]/div/div[11]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/h3').click()
 
 tabs = driver.window_handles
 driver.switch_to.window(tabs[1])
 
-time.sleep(4)
-d = driver.find_element(By.XPATH, '/html/body/mvid-root/div/mvid-primary-layout/mvid-layout/div/main/mvid-plp/mvid-product-list-block/mvid-filters-list/div/div[1]/div/div/mvid-accordion[3]/div/div/div/mvid-price-facet/div/mvid-range/div/input[1]')
-d.clear()
-d.send_keys('50000')
-d.send_keys(Keys.ENTER)
+time.sleep(10)
+driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div[3]/div[1]/div[6]/div/div/div[2]/label[14]').click()
+driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div/div[3]/div[2]/div/button[1]').click()
+
 time.sleep(30)
